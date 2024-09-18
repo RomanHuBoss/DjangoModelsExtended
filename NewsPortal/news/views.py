4187ff509017772b28863aca74b6d15bc0bc9e00
Roman Rabinovich <roman@web-line.ru>
2024-09-18 22:51:13 +0300

    Добавлены представления списка постов и отдельного поста

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = 'title'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'posts.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'posts_list'
+
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — product.html
+    template_name = 'post.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'post'
