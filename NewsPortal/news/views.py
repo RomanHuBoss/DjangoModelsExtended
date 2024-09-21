70a8f66a0cb017563dba907dc14c86f8b45c292e
Roman Rabinovich <roman@web-line.ru>
2024-09-21 08:49:53 +0300

    Коррекция согласно требованиям D3.3 ("Продолжаем постигать шаблоны")

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # Импортируем класс, который говорит нам о том,
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
@@ -8,13 +10,21 @@ class PostsList(ListView):
     # Указываем модель, объекты которой мы будем выводить
     model = Post
     # Поле, которое будет использоваться для сортировки объектов
-    ordering = 'title'
+    ordering = '-created_dt'
     # Указываем имя шаблона, в котором будут все инструкции о том,
     # как именно пользователю должны быть показаны наши объекты
     template_name = 'posts.html'
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'posts_list'
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.now()
+        context['next_publication'] = None
+        return context
 
 
 class PostDetail(DetailView):
